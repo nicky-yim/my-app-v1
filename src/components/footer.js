e6b87da87e7ffd13b665243c434e33e091b37132
Nicky Yim <nicky-yim@users.noreply.github.com>
2020-03-01 20:30:38 -0500

    Testing Netlify

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,6 @@ import footerStyle from '../styles/footer.module.scss'
 const Footer = () => {
     return (
         <footer className={footerStyle.footer}>
-
             <div className={footerStyle.footerContainer}>
                 <Icon iconName="FaEnvelope" href="mailto:nicky.yim@outlook.com" title="Email" />
                 <Icon iconName="FaGithub" href="https://github.com/nicky-yim" title="Github" />
