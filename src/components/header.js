a7e550f144fe1566dbe80d91e7f0df91cfcb5bfd
Nicky Yim <nicky-yim@users.noreply.github.com>
2020-03-02 23:37:12 -0500

    Mobile view and burger menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,37 +1,60 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { FaBars, FaTimes } from 'react-icons/fa'
 
 import headerStyle from '../styles/header.module.scss'
 
-const Header = () => {
-    return (
-        <header>
-            <nav>
-                <div className={headerStyle.logo}>
-                    <Link to="/">
-                        <h1>Nicky Yim</h1>
-                    </Link>
-                </div>
-                <ul className={headerStyle.navList}>
-                    <li>
-                        <Link to="/about" className={headerStyle.navItem} activeClassName={headerStyle.navItemActive}>
-                            About
+class Header extends React.Component {
+    constructor(props) {
+        super(props)
+        this.toggleMenu = this.toggleMenu.bind(this)
+        this.state = {
+            expanded: false
+        }
+    }
+
+    toggleMenu = () => {
+        this.setState({
+            expanded: !this.state.expanded
+        })
+    }
+
+    render() {
+        const menuIcon = this.state.expanded ? <FaTimes /> : <FaBars />
+        const navListClass = `${headerStyle.navList} ${this.state.expanded ? headerStyle.active : ''}`
+
+        return (
+            <header>
+                <nav>
+                    <div className={headerStyle.logo}>
+                        <Link to="/">
+                            <h1>Nicky Yim</h1>
                         </Link>
-                    </li>
-                    <li>
-                        <Link to="/projects" className={headerStyle.navItem} activeClassName={headerStyle.navItemActive}>
-                            Projects
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/contact" className={headerStyle.navItem} activeClassName={headerStyle.navItemActive}>
-                            Contact
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
+                        <button onClick={this.toggleMenu} className={headerStyle.burger}>
+                            {menuIcon}
+                        </button>
+                    </div>
+                    <ul className={navListClass}>
+                        <li>
+                            <Link to="/about" className={headerStyle.navItem} activeClassName={headerStyle.navItemActive}>
+                                About
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/projects" className={headerStyle.navItem} activeClassName={headerStyle.navItemActive}>
+                                Projects
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/contact" className={headerStyle.navItem} activeClassName={headerStyle.navItemActive}>
+                                Contact
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        )
+    }
 }
 
 export default Header
