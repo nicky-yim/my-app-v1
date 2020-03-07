3ae0d4fa966742c4f8f8641b4b442e4728653e08
Nicky Yim <nicky-yim@users.noreply.github.com>
2020-03-07 03:34:38 -0500

    Added about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,41 @@
 import React from 'react'
-import Layout from '../components/layout'
 
-import homeStyle from '../styles/index.module.scss'
+import Layout from '../components/layout'
+import Head from '../components/head'
+
+import MyAvatar from '../images/me.jpg'
+
+import aboutStyle from '../styles/about.module.scss'
 
 const About = () => {
     return (
         <Layout>
-            <div className={homeStyle.greeting}>
-                <h1>Work in progress... Sorry!</h1>
+            <Head title="About" />
+            <div className={aboutStyle.avatarContainer}>
+                <img src={MyAvatar} alt="Me" className={aboutStyle.avatar} />
+            </div>
+            <div className={aboutStyle.aboutMe}>
+                <h2>Hey, I'm <u>Nicky Yim</u>.</h2>
+                <p>
+                    I'm a software developer from Toronto, who specializes in front-end development.
+                </p>
+                <p>
+                    I began my journey as an <u>ASP.NET C#</u> software developer.
+                </p>
+                <p>
+                    Lately, I've been learning <u>React</u> in my free time. This site is proudly built by myself using <u>Gatsby</u>!
+                </p>
+                <p>
+                    Things I've been working with recently:
+                    <ul className={aboutStyle.list}>
+                        <li>ASP.NET</li>
+                        <li>C#</li>
+                        <li>HTML & (S)CSS</li>
+                        <li>JavaScript (ES6+)</li>
+                        <li>React</li>
+                        <li>Gatsby</li>
+                    </ul>
+                </p>
             </div>
         </Layout>
     )
